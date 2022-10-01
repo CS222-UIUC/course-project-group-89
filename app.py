f429229793e533062cf0d7679c89ae4036c9967a
Jessica Santhanam <13181862+jsanthanam10@users.noreply.github.com>
2022-10-01 00:04:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,16 @@ from datetime import datetime, timedelta, time
 app = Flask(__name__)
 
 # Route for "/" (frontend):
+#@app.route('/')
+# def index():
+#   return render_template("index.html")
+
 @app.route('/')
-def index():
-  return render_template("index.html")
+def dropdown():
+    cs_req = ["CS124", "CS128", "CS173", "MATH241", "MATH257", "CS210", "CS211", "CS222", "CS225", "CS233", "CS341", "CS357", "CS361", "CS374", "CS421"]
+    return render_template('index.html', cs_req=cs_req)
 
 def main():
-  print("hi")
+  app.run()
 #backend file
+
