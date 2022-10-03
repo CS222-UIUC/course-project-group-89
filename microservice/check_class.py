5d7d318988394cde541bd8c1cb2116515002ab84
Sammy <sammybassoon1516@hotmail.com>
2022-10-02 21:28:14 -0500

    commented out check_class for now

--- microservice/check_class.py
+++ microservice/check_class.py
@@ -15,47 +15,47 @@ from flask import Flask, jsonify
 
 
 # Open both as a pandas
-df_courses = pd.read_csv("courses.csv")
+# df_courses = pd.read_csv("courses.csv")
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-#Check to see if class exists --> Needs modifiying for our needs
+# #Check to see if class exists --> Needs modifiying for our needs
 
-@app.route('/<subject>/<number>/')
-def get_subject_number(subject, number):
-    '''Grabs course #'''
-    # Prep result:
-    result = { "course": f"{subject} {number}" }
+# @app.route('/<subject>/<number>/')
+# def get_subject_number(subject, number):
+#     '''Grabs course #'''
+#     # Prep result:
+#     result = { "course": f"{subject} {number}" }
 
-    # Cast `number` as an int and ensure `subject` is all caps:
-    try:
-        number = int(number)
-    except TypeError:
-        result["error"] = f"Course number `{number}` is not a number, try again"
-        status_code = 404
-        return jsonify(result), status_code
-    subject = subject.upper()
+#     # Cast `number` as an int and ensure `subject` is all caps:
+#     try:
+#         number = int(number)
+#     except TypeError:
+#         result["error"] = f"Course number `{number}` is not a number, try again"
+#         status_code = 404
+#         return jsonify(result), status_code
+#     subject = subject.upper()
 
-    # Fetch data:
-    courses = df_courses[ (df_courses["Subject"] == subject)
-                        & (df_courses["Number"] == number)
-                        & (df_courses["Start Time"] != "")
-                        & (df_courses["Start Time"] != "ARRANGED") ]
+#     # Fetch data:
+#     courses = df_courses[ (df_courses["Subject"] == subject)
+#                         & (df_courses["Number"] == number)
+#                         & (df_courses["Start Time"] != "")
+#                         & (df_courses["Start Time"] != "ARRANGED") ]
 
-    if len(courses) == 0:
-        # Provide an error:
-        result["error"] = f"No course data for {subject} {number}"
-        status_code = 404
-    else:
-        # Prefer LEC sections (for courses with discussions/labs)
-        course_lec = courses[ courses["Type Code"] == "LEC" ]
-        if len(course_lec) > 0:
-            courses = course_lec
+#     if len(courses) == 0:
+#         # Provide an error:
+#         result["error"] = f"No course data for {subject} {number}"
+#         status_code = 404
+#     else:
+#         # Prefer LEC sections (for courses with discussions/labs)
+#         course_lec = courses[ courses["Type Code"] == "LEC" ]
+#         if len(course_lec) > 0:
+#             courses = course_lec
 
-        # # Get the first result's data:
-        # selected_course = courses.iloc[0]
-        # result["Start Time"] = selected_course["Start Time"]
-        # result["Days of Week"] = selected_course["Days of Week"]
-        status_code = 200
+#         # # Get the first result's data:
+#         # selected_course = courses.iloc[0]
+#         # result["Start Time"] = selected_course["Start Time"]
+#         # result["Days of Week"] = selected_course["Days of Week"]
+#         status_code = 200
 
-    return jsonify(result), status_code
+#     return jsonify(result), status_code
