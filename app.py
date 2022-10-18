93b8d06e01b0e94e09ccfa13bd5ea0ecd9f447c6
Sammy <sammybassoon1516@hotmail.com>
2022-10-18 18:22:42 -0500

    Fixed index.html and worked on class.html

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ major_global = ""
 @app.route('/')
 def dropdown():
     """Function printing python version."""
-    cs_req = ["CS + GIS", "CS+Astro", "CS + STAT", "CS"]
+    cs_req = ["CS + GIS", "CS + Astro", "CS + STAT", "CS"]
     return render_template('index.html', cs_req=cs_req)
 
 @app.route('/major', methods=["POST"])
@@ -31,28 +31,41 @@ def main():
     return render_template("index.html")
 
 
-@app.route('/class', methods=["POST", "GET"])
-def classes():
-    df_cs =  ["CS124", "CS128", "CS173", "MATH241", "MATH257",
-    "CS210", "CS211", "CS222", "CS225", "CS233", "CS341", "CS357", "CS361", "CS374", "CS421"]
-    df_cs_stats =  ["CS124", "CS128", "CS173", "CS222", "CS225",
-    "MATH241", "CS233", "CS341", "CS340",  "CS357", "MATH257", "MATH415", "MATH416", "CS374",
-    "CS421", "STAT107", "STAT200", "STAT212", "STAT400", "STAT410", "STAT425", "STAT426"]
-    df_cs_astronomy =  ["CS124", "CS128", "CS173", "CS222",
-    "CS225",  "CS233", "CS341", "CS340", "STAT200", "STAT212", "CS361", "CS374", "CS421", "MATH221",
-    "MATH220", "MATH225", "MATH257", "MATH231", "PHYS211", "PHYS212", "MATH241",
-    "ASTR210", "ASTR310", "ASTR404", "ASTR405", "ASTR406", "ASTR414"]
+@app.route('/class', methods=["GET"])
+def checkboxes():
+    df_cs =  ["CS 124", "CS 128", "CS 173", "MATH 241", "MATH 257",
+    "CS 210", "CS 211", "CS 222", "CS 225", "CS 233", "CS 341", "CS 357", "CS 361", "CS 374", "CS 421"]
+
+    df_cs_stats =  ["CS 124", "CS 128", "CS 173", "CS 222", "CS 225",
+    "MATH241", "CS 233", "CS 341", "CS 340",  "CS 357", "MATH 257", "MATH 415", "MATH 416", "CS 374",
+    "CS 421", "STAT 107", "STAT 200", "STAT 212", "STAT 400", "STAT 410", "STAT 425", "STAT 426"]
+
+    df_cs_astronomy =  ["CS 124", "CS 128", "CS 173", "CS 222",
+    "CS 225",  "CS 233", "CS 341", "CS 340", "STAT 200", "STAT 212", "CS 361", "CS 374", "CS 421", "MATH 221",
+    "MATH 220", "MATH 225", "MATH 257", "MATH 231", "PHYS 211", "PHYS 212", "MATH 241",
+    "ASTR 210", "ASTR 310", "ASTR 404", "ASTR 405", "ASTR 406", "ASTR 414"]
+
+    df_cs_ggis = ["CS 124", "CS 128", "CS 173", "CS 222", "CS 225","CS 211", "CS 233", "CS 341", "CS 340",
+     "CS 374", "CS 421", "MATH 231", "MATH 257", "STAT 200","STAT 212", "CS 361", "GGIS 371", "GGIS 379",
+      "GGIS 380"]
+
     print("Here: ", major_global)
     if (major_global == "CS"):
         return render_template("class.html", cs_req= df_cs)
-
     elif (major_global == "CS + STATS"):
         return render_template("class.html", cs_req= df_cs_stats)
-
+    elif (major_global == "CS + GIS"):
+        return render_template("class.html", cs_req= df_cs_ggis)
     else:
         return render_template("class.html", cs_req= df_cs_astronomy)
 
 
+
+@app.route('/class', methods=["POST"])
+def classes():
+    classes_taken = request.form["class"]
+    print("classes_taken: ", classes_taken)
+    return render_template("class.html")
 
 
 
