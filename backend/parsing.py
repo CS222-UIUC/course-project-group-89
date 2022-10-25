66f07e1778e12c9a141d7e154b256fdf27289f3f
Emily Ho <eeho2@illinois.edu>
2022-10-24 21:36:49 -0500

    attempting to connect front and back end for checkboxes

--- backend/parsing.py
+++ backend/parsing.py
@@ -1,23 +1,28 @@
 "'this module reades into the courses.csv'"
 from queue import Empty
+
 import pandas as pd
+
 import course_requirements
 
 df_ = pd.read_csv('backend/courses.csv')
 
 df_['Subject and Number'] = df_['Subject'] + df_['Number'].map(str)
-df_ = df_.drop(columns = ['Subject', 'Number'])
+# df_ = df_.drop(columns = ['Subject', 'Number'])
 
 def sort_core_classes(major_):
     '''returns core classes given a major'''
     df_user_information = pd.DataFrame(columns = df_.columns.tolist())
     core_course_list = Empty
-    if major_ == "cs + ggis":
+    if major_ == "CS + GGIS":
         core_course_list = course_requirements.df_cs_ggis.values.tolist()
-    elif major_ == "cs + stat":
+    elif major_ == "Stats & CS":
         core_course_list = course_requirements.df_cs_stats.values.tolist()
-    elif major_ == "cs + astronomy":
+    elif major_ == "CS + ASTRO":
         core_course_list = course_requirements.df_cs_astronomy.values.tolist()
+    elif major_ == "CS":
+        core_course_list = course_requirements.df_cs.values.tolist()
+
     for course in core_course_list:
         entry = df_.loc[df_['Subject and Number'] == course[0]]
         df_user_information = pd.concat([df_user_information, entry])
