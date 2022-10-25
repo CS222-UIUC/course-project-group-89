25e680a6d18d90699e288b1f58416533e707f806
Sammy <sammybassoon1516@hotmail.com>
2022-10-24 23:04:36 -0500

    clean

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
-'''this module renders a template and has two functions (testing for emily 10/1/22)'''
-
-# import os
-# from datetime import datetime, timedelta, time
-from backend.parsing import sort_core_classes
 from flask import Flask, render_template, request
-# request, jsonify
-# import requests
+import os
+from parsing import sort_core_classes
 
 app = Flask(__name__)
 
-# MAJOR_GLOBAL = ""
+# Route for "/" (frontend):
 @app.route('/')
 def dropdown():
     """Function printing python version."""
+    print(os.getcwd())
     cs_req = ["CS + GGIS", "CS + ASTRO", "Stats & CS", "CS"]
     return render_template('index.html', cs_req=cs_req)
 
+# Getting major of first user for "/major" (frontend):
 @app.route('/major', methods=["POST"])
 def main():
     "'this function is for main'"
@@ -31,7 +28,7 @@ def main():
     #backend file
     return render_template("index.html")
 
-
+# Getting major of second user for "/friendmajor" (frontend):
 @app.route('/friendmajor', methods=["POST", "GET"])
 def friendmajor():
     #Have backend team return the info in this function
@@ -40,25 +37,17 @@ def friendmajor():
     cs_req = ["CS + GIS", "CS + ASTRO", "CS + STAT", "CS"]
     return render_template('friendmajor.html', cs_req=cs_req)
 
-
 @app.route('/class', methods=["POST", "GET"])
 def classes():
     """Function printing python version."""
-    df_cs =  ["CS124", "CS128", "CS173", "MATH241", "MATH257",
-    "CS210", "CS211", "CS222", "CS225", "CS233", "CS341", "CS357", "CS361", "CS374", "CS421"]
-    df_cs_stats =  ["CS124", "CS128", "CS173", "CS222", "CS225",
-    "MATH241", "CS233", "CS341", "CS340",  "CS357", "MATH257", "MATH415", "MATH416", "CS374",
-    "CS421", "STAT107", "STAT200", "STAT212", "STAT400", "STAT410", "STAT425", "STAT426"]
-    df_cs_astronomy =  ["CS124", "CS128", "CS173", "CS222",
-    "CS225",  "CS233", "CS341", "CS340", "STAT200", "STAT212", "CS361", "CS374", "CS421", "MATH221",
-    "MATH220", "MATH225", "MATH257", "MATH231", "PHYS211", "PHYS212", "MATH241",
-    "ASTR210", "ASTR310", "ASTR404", "ASTR405", "ASTR406", "ASTR414"]
-    place_holder = ""
-    if place_holder == "CS":
-        return render_template("class.html", cs_req= df_cs)
-    if place_holder == "CS + STATS":
-        return render_template("class.html", cs_req= df_cs_stats)
-    return render_template("class.html", cs_req= df_cs_astronomy)
+    major = ""
+    with open("store_user_input.txt", "r", encoding="utf8") as curr_file:
+        major = curr_file.readline().rstrip()
+    print("in checkboxes(): ", major)
+    cs_req = []
+    cs_req = sort_core_classes(major)
+    curr_file.close()
+    return render_template("class.html", cs_req= cs_req)
 
 @app.route('/friendclass', methods=["POST", "GET"])
 def friendclasses():
